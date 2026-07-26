8061da6ccba892bcbe9c811e6bb3391323f4ad6c
StepanxXx <stepanxxx@gmail.com>
2026-07-27 02:00:24 +0300

    refactor: clean up component code formatting and standardize style quotes

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,11 +1,8 @@
 // Notification
 import styles from "./Notification.module.css";
 
-
 function Notification() {
-  return (
-    <p className={styles.message}>No feedback yet</p>
-  );
+  return <p className={styles.message}>No feedback yet</p>;
 }
 
 export default Notification;
